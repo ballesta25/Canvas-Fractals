fa104e2402564a1095b8b6630d5d305e2989d9f4
Andrei Darien Elliott <a.d.elliott@ku.edu>
2015-12-13 01:07:10 -0600

    Added Cantor set, showing stages at offset, unions of known fractals, and Koch snowflake.  Should probably switch Fractal from a type class to a type.

--- fractal.hs
+++ fractal.hs
@@ -52,30 +52,58 @@ instance Fractal LineFractal where
     approximate (LineFractal l _)    = drawLine l
     step        (LineFractal _ next) = next
 
--- draw the approximations for the fractal a given depth (only at that depth, 
+data UnionFractal where
+    (:+)  :: (Fractal f, Fractal g) => f -> g -> UnionFractal
+    Empty :: UnionFractal
+
+instance Fractal UnionFractal where
+    approximate (f :+ g) = approximate f >> approximate g
+    approximate Empty    = return ()
+    step        (f :+ g) = map (:+ Empty) (step f) ++
+                           map (Empty :+) (step g) -- inefficient
+    step        Empty    = []
+
+-- draw the approximations for the fractal a given depth (only at that depth,
 --   no previous levels are drawn)
-drawLeaves :: Fractal f => Integer -> f -> Canvas ()
+drawLeaves :: Fractal f => Int -> f -> Canvas ()
 drawLeaves 0     = approximate
 drawLeaves depth = mapM_ (drawLeaves (depth - 1)) . step
 
 -- recurrence for the Koch Snowflake
 {-
-
                             e
     a---------b  --->      / \
                        a--c   d--b
-
 -}
 koch :: Line -> [Line]
 koch (a, b) = let r   = dist  a b
                   phi = angle a b
                   c   = liftPair (+) a (polar (r/3) phi)
                   d   = liftPair (+) a (polar (2*r/3) phi)
-                  e   = liftPair (+) c (polar (r/3) (phi + (pi/3)))
+                  e   = liftPair (+) c (polar (r/3) (phi - pi/3))
                   chain = [a,c,e,d,b]
               in zip chain (tail chain)
 
+snowflake :: Line -> UnionFractal
+snowflake (a, b) = let c = liftPair (+) a $
+                           polar (dist a b) $ (angle a b) + pi/3
+                   in foldr (:+) Empty $ map (mkFractal koch) [(a, b), (b, c), (c, a)]
+
+-- has the Cantor set as its limit
+cantor :: Line -> [Line]
+cantor (a, b) = [(a, oneThirdFromTo a b), (b, oneThirdFromTo b a)]
+    where oneThirdFromTo = liftPair (\x y -> (x+x + y)/3)
+
+-- Draw several iterations of a fractal at given offset from each other
+generations :: Fractal f => f -> Int -> Point -> Canvas ()
+generations f limit offset = sequence_ 
+            [drawLeaves n f >> translate offset | n <- [0..limit-1]] 
+            >> translate (undo * fst offset, undo * snd offset)
+            where undo = (-1) * fromIntegral limit -- remove the offset from later drawings
+
 main :: IO()
 main = blankCanvas 3000 $ \context -> do 
-         send context $ drawLeaves 5 $ mkFractal koch ((100,200),(1000,200))
-           
+         send context $ do 
+           drawLeaves 5 $ mkFractal koch ((100,400),(1000,400))
+           generations (mkFractal cantor ((200, 500), (1200, 500))) 5 (0, 50)
+           drawLeaves 4 $ snowflake ((1400,200),(1500,200))
